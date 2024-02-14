76c105c5c7256578b637be8ef0b1c57eb34c74cb
Brian Howatt <brianhowatt@Brians-MacBook-Air.local>
2024-02-13 23:38:23 -0600

    added regression analysis and model plot

--- test_analysis.R
+++ test_analysis.R
@@ -5,5 +5,18 @@ data(iris)
 summary(iris)
 
 # plot some raw data
+plot(iris$Sepal.Length, iris$Petal.Width)
+
+# run a regression
+res <- lm(Petal.Width ~ Sepal.Length, data = iris)
+summary(res)
+
+# add the model fit to the plot
+abline(res, col = 'red')
+
+## What if we add in Species?
 plot(iris$Sepal.Length, iris$Petal.Width, col = factor(iris$Species))
 
+# run a regression
+res <- lm(Petal.Width ~ Sepal.Length * Species, data = iris)
+summary(res)
